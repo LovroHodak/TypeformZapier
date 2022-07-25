27da7b5deab71cdabbd5d31dd205e5ee50e43060
Lovro <hodak.lovro@gmail.com>
2022-07-25 14:02:21 +0200

    success

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { Widget } from "@typeform/embed-react";
 import data from './content/testing1.json'
-console.log(data.testing1[0])
+console.log(data.testing1)
 
 function App() {
   return (
@@ -9,12 +9,24 @@ function App() {
       <Widget id="DZ4cpBuy" style={{height: 600, width: '100%'}}/>
       <div>
         <h1>ALL DATA UNDER IS FROM NETLIFY CMS</h1>
-        <p>{data.testing1[0].label}</p>
+        {data.testing1.map((item, i) => {
+          return (
+            <div key={i}>
+              <p>{item.label}</p>
+              <p>{item.date}</p>
+              <p>{item.title}</p>
+              <img src={item.thumbnail} />
+              <p>{item.rating}</p>
+              <p>{item.body}</p>
+            </div>
+          )
+        })}
+        {/* <p>{data.testing1[0].label}</p>
         <p>{data.testing1[0].date}</p>
         <p>{data.testing1[0].title}</p>
         <img src={data.testing1[0].thumbnail} />
         <p>{data.testing1[0].rating}</p>
-        <p>{data.testing1[0].body}</p>
+        <p>{data.testing1[0].body}</p> */}
       </div>
     </div>
   );
