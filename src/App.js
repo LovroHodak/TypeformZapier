cf9bebd19d29eeb284294712d4b194b2f8914b71
Lovro <hodak.lovro@gmail.com>
2022-07-25 12:48:03 +0200

    changed project name

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Widget } from "@typeform/embed-react";
 function App() {
   return (
     <div style={{height: '100%', width: '100%'}}>
+      <h1>Lovoro</h1>
       <Widget id="DZ4cpBuy" style={{height: 600, width: '100%'}}/>
     </div>
   );
