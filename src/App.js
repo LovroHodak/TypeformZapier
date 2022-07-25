828b2675578cd5801080356b48376a86c42d20b1
Lovro <hodak.lovro@gmail.com>
2022-07-25 13:54:52 +0200

    mission success

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import { Widget } from "@typeform/embed-react";
+import data from './content/testing1.json'
+console.log(data.testing1[0])
 
 function App() {
   return (
     <div style={{height: '100%', width: '100%'}}>
       <h1>Lovoro</h1>
       <Widget id="DZ4cpBuy" style={{height: 600, width: '100%'}}/>
+      <div>
+        <h1>ALL DATA UNDER IS FROM NETLIFY CMS</h1>
+        <p>{data.testing1[0].label}</p>
+        <p>{data.testing1[0].date}</p>
+        <p>{data.testing1[0].title}</p>
+        <img src={data.testing1[0].thumbnail} />
+        <p>{data.testing1[0].rating}</p>
+        <p>{data.testing1[0].body}</p>
+      </div>
     </div>
   );
 }
